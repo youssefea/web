8ec0fa17d67bc7e76dfd5dab7a2670bb06385c20
Brendan from DeFi <brendan.forster@coinbase.com>
2025-03-27 14:49:08 -0400

    chore: replace vercel-kv with ioredis (#2049)
    * replace vercel/kv with ioredis
    
    * private getClient
    
    * remove vercel/kv
    
    * renamed file, added error handling
    
    * update kv usage
    
    * add SetCommandOptions type explainer
    
    * simplify set method
    
    * rename env vars
    
    * update kvManager, import paths
    
    * remove console logs
    
    * remove console errors
    
    * remove unnecessary awaits, fix lints
    
    * add back awaits for linter
    
    * rename index file

--- apps/web/pages/api/checkNftProof/index.ts
+++ apps/web/pages/api/checkNftProof/index.ts
@@ -1,5 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
-import { kv } from '@vercel/kv';
+import { kv } from 'apps/web/src/utils/kv';
 import { logger } from 'apps/web/src/utils/logger';
 
 type RequestBody = {
