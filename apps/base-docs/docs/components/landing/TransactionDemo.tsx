78b3536b69d476b4fb66efc38d102ffd734aaeb7
Alissa Crane <alissacranespam@gmail.com>
2025-03-28 12:46:23 -0700

    chore: update transaction docs (#2041)
    * update transaction docs
    
    * remove reference to txn default

--- apps/base-docs/docs/components/landing/TransactionDemo.tsx
+++ apps/base-docs/docs/components/landing/TransactionDemo.tsx
@@ -13,7 +13,7 @@ import TransactionWrapper from '../TransactionWrapper.tsx';
 
 export const transactionDemoCode = `
   import { useAccount } from 'wagmi';
-  import { TransactionDefault } from '@coinbase/onchainkit/transaction';
+  import { Transaction } from '@coinbase/onchainkit/transaction';
   import { WalletDefault } from '@coinbase/onchainkit/wallet';
 
   export default function TransactionDemo() {
@@ -40,7 +40,7 @@ export const transactionDemoCode = `
     return (
       <>
         {address ? (
-          <TransactionDefault contracts={contracts} chainId={84532} />
+          <Transaction contracts={contracts} chainId={84532} />
         ) : (
           <WalletDefault />
         )}
